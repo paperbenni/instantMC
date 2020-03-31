69f2a1a966d7fc70511c285ce7241539271e8e5a
paperbenni <paperbenni@gmail.com>
2020-04-01 01:19:25 +0200

    add kubectl command

--- kube/reboot.sh
+++ kube/reboot.sh
@@ -4,4 +4,8 @@ if ! command -v instantmc; then
     curl -s 'https://raw.githubusercontent.com/paperbenni/instantMC/master/local/install.sh' | sh
     export TERM=xterm
     screen -d -m "instantmc"
+    sleep 10
+    if ! pgrep instantmc; then
+        screen -d -m "instantmc"
+    fi
 fi
--- kube/watch.sh
+++ kube/watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PODNAME="$(kubectl get pods | tail -1 | grep -o '^[^ ]*')"
-export TERM=xterm
+kubectl exec -it "$PODNAME" -- /bin/bash -c "apk add curl bash && curl https://raw.githubusercontent.com/paperbenni/instantMC/master/kube/reboot.sh | bash"
